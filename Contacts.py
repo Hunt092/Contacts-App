af1e7710d739d9aff6e7145ff44f6be1e4eeda65
Hunt092 <hunters.hunt92@gmail.com>
2020-08-13 19:50:55 +0530

    Made Modification page Functional Partly

--- Contacts.py
+++ Contacts.py
@@ -10,7 +10,7 @@ ButtonFont = ('Impact',15,)
 EntryFont = ('Arial', 16)
 TextFont= ('Impact',20,)
 #-----------------#
-
+i=0
 
 # Opening the Json File if it doesnt exist and empty list is used
 ContactList=[]
@@ -31,7 +31,7 @@ def GetData(name,numbers,addresses,emails):
 
 def MakeContact(Name,Numbers,Addresses,Emails):
     try:
-        name,numbers,addresses,emails = GetData(Name.get(),Numbers.get(),Addresses.get(),Emails.get())
+        name,numbers,addresses,emails = GetData(Name.get().strip(),Numbers.get().strip(),Addresses.get().strip(),Emails.get().strip())
         Name.delete(0, 'end')
         Numbers.delete(0, 'end')
         Addresses.delete(0, 'end')
@@ -78,11 +78,35 @@ def SaveContact(Contact):
     ContactList.append(Contact)
     with open("ContactList.json", 'w') as f:
         json.dump(ContactList,f)
-def OpenNewPage(canvasname,Titlename,NextpageName=None):
+
+def OpenNewPage(canvasname,Titlename,NextpageName=None,ContactIndex=None):
+    
     canvasname.destroy()
     Titlename.destroy()
+    print(NextpageName)
+    if NextpageName!= None and ContactIndex!= None:
+        NextpageName(ContactIndex)
     if NextpageName!= None:
-        NextpageName()
+        try:
+            NextpageName()
+        except:
+            pass
+
+def GetIndex(Listname):
+    try:
+        return Listname.curselection()[0]
+    except:
+        return None
+
+def EditContactPages(canvas1,Title,Index):
+    if Index==None:
+        OpenNewPage(canvas1,Title,SearchContactPage)
+    else:    
+        OpenNewPage(canvas1,Title,Modificationpage,Index)
+
+# def UpdateContact(Index,Name,Number,Address,Emails):
+#     ContactList[Index].update(Name.get().strip():{"Number":Number.get().strip().split(',')},
+#     {"Address":Address.get().strip()},{"Email:"Emails.get().strip().split(',')})
 #--------------------------------------#
 
 
@@ -91,9 +115,9 @@ def NewContact():
    
     Title= tk.Label(root, text="New Contact",font=TitleFont,pady=10,padx=10,bd=4,relief = 'raised')
     Title.pack(side='top')
+
     Canvas= tk.Canvas(root, height=HEIGHT, width = WIDTH, bg='#ffff00')
     Canvas.pack()
-
 
     NameLabel= tk.Label(Canvas,text="Name:",font=TextFont, bg='#ffff00')
     NameLabel.place(relx=0.02,rely=0.02)
@@ -114,11 +138,9 @@ def NewContact():
     Number= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
     Number.place(relx=0.02,rely=0.3)
     
-
     Address= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
     Address.place(relx=0.02,rely=0.5)
     
-
     Emails= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
     Emails.place(relx=0.02,rely=0.7) 
     
@@ -137,7 +159,6 @@ def OpenScreen():
     canvas1 = tk.Canvas(root, height=HEIGHT, width = WIDTH, bg='#ffff00')
     canvas1.pack()
 
-    
     frame= tk.Frame(canvas1,bg='#b3b3b3',relief='raised',bd=1,)
     frame.place(relx=0.0,rely=0.0,relheight=0.8,relwidth=1,)
 
@@ -146,7 +167,7 @@ def OpenScreen():
     New.place(relx=0.025, rely=0.8,relwidth=0.25)
 
     Edit=  tk.Button(canvas1,text="Edit a Contact",  pady=10, 
-                    font= ButtonFont, command = lambda :OpenNewPage(canvas1,Title,EditContact))
+                    font= ButtonFont, command = lambda :EditContactPages(canvas1,Title,GetIndex(Appname)))
     Edit.place(relx=0.725, rely=0.8,relwidth=0.25)
 
     Exit = tk.Button(canvas1,text="Exit", pady=1, font= ButtonFont,command=lambda :root.quit())
@@ -154,13 +175,61 @@ def OpenScreen():
 
     scrollbar = tk.Scrollbar(frame)
     scrollbar.pack( side = 'right', fill = 'y' )
+
     Appname = tk.Listbox(frame,yscrollcommand = scrollbar.set,font=TextFont,bg='#b3b3b3',)
     for contact in ContactList:
       for name in contact:
             Appname.insert('end',f" {name} : {contact[name]['Number'][0]}")
+            
     Appname.place(relx=0,rely=0,relheight=1,relwidth=1,)
 
-def EditContact():
+
+
+
+def Modificationpage(Index):
+    Title= tk.Label(root, text="Edit Contact",font=TitleFont,pady=10,padx=10,bd=4,relief = 'raised')
+    Title.pack(side='top')
+
+    Canvas= tk.Canvas(root, height=HEIGHT, width = WIDTH, bg='#ffff00')
+    Canvas.pack()
+
+    NameLabel= tk.Label(Canvas,text="Name:",font=TextFont, bg='#ffff00')
+    NameLabel.place(relx=0.02,rely=0.02)
+
+    NumLabel= tk.Label(Canvas,text="Number:",font=TextFont, bg='#ffff00')
+    NumLabel.place(relx=0.02,rely=0.22)
+
+    AdLabel= tk.Label(Canvas,text="Address:",font=TextFont, bg='#ffff00')
+    AdLabel.place(relx=0.02,rely=0.42)
+
+    ELabel= tk.Label(Canvas,text="Email:",font=TextFont, bg='#ffff00')
+    ELabel.place(relx=0.02,rely=0.62)
+
+    Name= tk.Entry(Canvas, width= round(WIDTH*0.04), font=EntryFont )
+    Name.insert('end',(list(ContactList[Index])[0]))
+    Name.place(relx=0.02 , rely=0.1)
+    
+    Number= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
+    Number.insert('end',ContactList[Index][list(ContactList[Index])[0]]['Number'])
+    Number.place(relx=0.02,rely=0.3)
+    
+    Address= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
+    Address.insert('end',ContactList[Index][list(ContactList[Index])[0]]['Address'])
+    Address.place(relx=0.02,rely=0.5)
+    
+    Emails= tk.Entry(Canvas,width= round(WIDTH*0.04), font=EntryFont)
+    Emails.insert('end',ContactList[Index][list(ContactList[Index])[0]]['Email'])
+    Emails.place(relx=0.02,rely=0.7) 
+
+    Submit= tk.Button(Canvas,text="Update",font=ButtonFont,padx=30,pady=10,bg=None)
+                        #command= lambda : UpdateContact(Index,Name,Number,Address,Emails))
+    Submit.place(relx=0.7,rely=0.8)
+
+    Back =  tk.Button(Canvas,text="Back",padx=20,font=ButtonFont,
+                         command = lambda : OpenNewPage(Canvas,Title,OpenScreen))
+    Back.place(relx=0.875,rely=0.01)
+
+def SearchContactPage():
     Title= tk.Label(root, text="Edit Contact",font=TitleFont,pady=10,padx=10,bd=4,relief = 'raised')
     Title.pack(side='top')
 
@@ -171,19 +240,21 @@ def EditContact():
     Text.place(relx=0.225 , rely=0.4)
 
     Search= tk.Entry(Canvas, width= round(WIDTH*0.04), font=EntryFont)
-    
     Search.place(relx=0.25 , rely=0.5)
     
-    SearchButton = tk.Button(text="Edit a Contact", padx =30 , pady=15, 
+    SearchButton = tk.Button(Canvas,text="Edit a Contact", padx =30 , pady=15, 
                     font= ButtonFont,)
     SearchButton.place(relx=0.38,rely=0.6)
-
+    
     Back =  tk.Button(Canvas,text="Back",padx=20,font=ButtonFont, command = lambda : OpenNewPage(Canvas,Title,OpenScreen))
     Back.place(relx=0.875,rely=0.01)
 
-if __name__ =='__main__':
-    root = tk.Tk()
-    root.title("Contacts")
-    OpenScreen()
 
-    root.mainloop()
+if __name__ =='__main__':
+    # root = tk.Tk()
+    # root.title("Contacts")
+    # OpenScreen()
+    
+    # root.mainloop()
+
+    ContactList[0].update("Yash",21)
